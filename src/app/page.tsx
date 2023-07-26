dd260756d4880696683f2684212a213d5bcb1525
Edson Li <edsonli1999@gmail.com>
2023-07-26 19:22:34 +1000

    Built header section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
 import Image from 'next/image'
+import { Metadata } from 'next'
+import Header from './components/Header'
+ 
+export const metadata: Metadata = {
+  title: "Edson's Portfolio",
+}
+
+// Note: Sonny uses nextjs version (12.3.0) as opposed to my nextjs version (13.4.12)
+// Which is why the folder structure (and other small stuff) is slightly different
+
+// VsCode Extensions:
+// TailwindCSS intellisense
+// ES7+ react extension
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold">
-      Edson's portfolio :)))
-    </h1>
+    <div>
+    
+      {/* Header */}
+      <Header/>
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact Me */}
+
+    </div>
+
   )
 }

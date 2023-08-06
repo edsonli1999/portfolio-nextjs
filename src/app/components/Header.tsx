07aadb9fa64f33cd8b73acd124eb471939b42f69
Edson Li <edsonli1999@gmail.com>
2023-08-06 22:08:35 +1000

    Finished up header animations, started Hero part

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,7 @@
+"use client"
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
+import { motion } from "framer-motion"
 
 type Props = {}
 
@@ -7,7 +9,11 @@ function Header({}: Props) {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 
     xl:items-center'> 
-      <div className='flex flex-row items-center'>
+      <motion.div 
+      initial={{ x: -500, opacity: 0, scale: 0.5 }}
+      animate={{ x: 0, opacity: 1, scale: 1 }}
+      transition={{ duration: 1.5,}}
+      className='flex flex-row items-center'>
         {/* Social Icons */}
         <SocialIcon 
           url="https://www.linkedin.com/in/edson-li-387i/" 
@@ -24,9 +30,13 @@ function Header({}: Props) {
           fgColor='gray' 
           bgColor='transparent'
         />
-      </div>
+      </motion.div>
 
-      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+      <motion.div 
+      initial={{ x:500, opacity:0, scale:0.5}}
+      animate={{ x:0, opacity:1, scale:1}}
+      transition={{ duration: 1.5}}
+      className='flex flex-row items-center text-gray-300 cursor-pointer'>
         <SocialIcon
             className='cursor-pointer'
             network='email'
@@ -34,7 +44,7 @@ function Header({}: Props) {
             bgColor='transparent'
           />
         <p className='uppercase hidden md:inline-flex text-sm text-gray-400'> Get in touch </p>
-      </div>
+      </motion.div>
 
     </header>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import Image from 'next/image'
 import { Metadata } from 'next'
 import Header from './components/Header'
- 
-export const metadata: Metadata = {
-  title: "Edson's Portfolio",
-}
 
 // Note: Sonny uses nextjs version (12.3.0) as opposed to my nextjs version (13.4.12)
 // Which is why the folder structure (and other small stuff) is slightly different
@@ -13,14 +9,19 @@ export const metadata: Metadata = {
 // TailwindCSS intellisense
 // ES7+ react extension
 
+export const metadata: Metadata = {
+  title: "Edson's Portfolio",
+}
+
 export default function Home() {
   return (
     <div>
-    
-      {/* Header */}
       <Header/>
-      {/* Hero */}
 
+      {/* Hero */}
+      <section id="hero">
+        <Hero/>
+      </section>
       {/* About */}
 
       {/* Experience */}

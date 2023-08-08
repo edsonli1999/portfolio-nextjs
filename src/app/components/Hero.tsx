b0c63f7ed20200916082a8ea75ec65c8936d2df8
Edson Li <edsonli1999@gmail.com>
2023-08-08 17:47:18 +1000

    Part 1/2 of Hero section, text and pulsing circle

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,10 +1,31 @@
+"use client"
 import React from 'react'
+import { Cursor, Typewriter, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles';
+
 
 type Props = {}
 
+// Uses a typewriter library for the typewriter effect, Sonny uses react-simple-typewriter.
+
 function Hero({}: Props) {
+  //useEffect thing from the typewriter library
+  const [text, count] = useTypewriter({
+    words: ["Hi, my name's Edson", 
+    "Guy-who-boulders.tsx", 
+    "<ButLovesToCodeMore/>"],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
   return (
-    <div>Hero</div>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'> 
+      <BackgroundCircles/>
+      <h1>
+        <span> {text} </span>
+        <Cursor cursorColor='#1e95aa'/>
+      </h1>
+    </div>
   )
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image'
 import { Metadata } from 'next'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
-// Note: Sonny uses nextjs version (12.3.0) as opposed to my nextjs version (13.4.12)
-// Which is why the folder structure (and other small stuff) is slightly different
+// Note: 
+// 1. Sonny uses nextjs version (12.3.0) as opposed to my nextjs version (13.4.12) 
+//    Which is why the folder structure (and other small stuff) is slightly different
+// 2. Sonny doens't seem to need to add in "use client" in his component files.
 
 // VsCode Extensions:
 // TailwindCSS intellisense
@@ -15,11 +18,12 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Header/>
 
       {/* Hero */}
-      <section id="hero">
+      {/* To make snap work, need to make parent snap mandatory */}
+      <section id="hero" className='snap-center'> 
         <Hero/>
       </section>
       {/* About */}

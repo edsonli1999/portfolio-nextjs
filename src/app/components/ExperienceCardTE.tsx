1d8636f61e0a4e238dc315192d07cfede3efae36
Edson Li <edsonli1999@gmail.com>
2023-10-13 18:29:15 +1100

    Minor changes to wording and text colour in experience section

--- src/app/components/ExperienceCardTE.tsx
+++ src/app/components/ExperienceCardTE.tsx
@@ -35,15 +35,43 @@ function ExperienceCardTE({}: Props) {
           
 
         </div>
-        <p className='uppercase py-5 text-gray-300'> Dec 2022 - Feb 2023 </p>
+        <p className='uppercase py-4 text-gray-300'> Dec 2022 - Feb 2023 </p>
       
-        {/* Make this scrollable container */}
-        <ul className='list-disc space-y-4 ml-5 text-md'>
+        <ul className='list-disc space-y-4 ml-5 text-md text-gray-200'>
           <li> Support the development of a polling web app used by the Singapore Government in the recent Presidential Election. </li>
           <li> Duties include manual testing, updating test datasets, writing scripts for user acceptance testing, updating database models, and other various administrative tasks.</li>
           <li> Technology used: Data cleaning (excel, python pandas), scripting (python selenium), database modelling (draw.io, AWS), admin (microsoft office) </li>
           <li> Reference: Wei Yang +65 9029 1959</li>
         </ul>
+
+        <div className='py-5'>
+          <p className='py-1 uppercase text-gray-300'> 
+            Personal summary:
+          </p>
+
+          <p className='py-2 text-gray-200'> 
+          I was given a testing script from a full-time developer, coded using Python Selenium. Although functional, the previous developer was in a time crunch, and as such I was tasked to make it more stable, user friendly, and reusable. Additionally, I had to make a new script that was similar, with its purpose to test another part of the website.
+          </p>
+
+          <p className='py-2 text-gray-200'> 
+          After some brainstorming, I decided to first split the single python file with 10,000 lines into multiple files, as well as add in the function of taking in multiple parameters when running to account for different testing scenarios.
+          </p>
+
+          
+          <p className='py-2 text-gray-200'> 
+          I also made it possible to start from an intermediate step (in the user story), for situations when the testing script stopped at a certain step, for whatever reason.
+          </p>
+
+          <p className='py-2 text-gray-200'> 
+          Lastly, I introduced the idea of using a screen recorder while running the testing script. This improved efficiency, as there were situations where we had to check the recording and align the scenario with what was recorded in the database. Previously, the developers relied on their memory of what the script executed on the website. My use of the screen recorder got rid of the possibility for human error, making sure that every single step was recorded and accounted for.
+          </p>
+
+          <p className='py-2 text-gray-200'> 
+          Ultimately, my internship period was over before the project deadline. However, I was told from my coworkers who stayed on that they continued with my methods, with minimal changes to my code. The project eventually completed, to which I have faith that my contributions have made its testing much more efficient.
+          </p>
+        </div>
+        
+
       </div>
 
       
